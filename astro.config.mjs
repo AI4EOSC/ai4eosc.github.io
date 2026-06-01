5515f87fd8ed9f35597fb891e527a52ce27cc72f
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2026-06-01 12:56:16 +0200

    fix: Update site URL and add base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import yaml from "@rollup/plugin-yaml";
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ai4eosc.eu",
+	site: "https://ai4eosc.github.io",
+	base: "ai4eosc-initiative-web",
 	integrations: [alpinejs(), sitemap()],
 	vite: {
 		plugins: [tailwindcss(), yaml()],
