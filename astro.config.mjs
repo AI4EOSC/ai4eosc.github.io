17e32e8b3b265aeb5a9dc2d3c70c279f54574565
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2026-06-01 13:00:33 +0200

    fix: Remove base configuration from astro.config.mjs
    Removed base configuration for Astro site.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ai4eosc.github.io",
-	base: "ai4eosc-initiative-web",
 	integrations: [alpinejs(), sitemap()],
 	vite: {
 		plugins: [tailwindcss(), yaml()],
